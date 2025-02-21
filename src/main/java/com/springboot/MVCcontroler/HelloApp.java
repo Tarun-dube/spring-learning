210baae57261b76687d11ffab934e361c940917e
Tarun Dubey <tarun2019dubey@gmail.com>
2025-02-21 23:56:37 +0530

    [tarun] Add. hello app mvc

---
+++ src/main/java/com/springboot/MVCcontroler/HelloApp.java
@@ -0,0 +1,14 @@
+package com.springboot.MVCcontroler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloApp {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(HelloApp.class, args);
+    }
+
+}
