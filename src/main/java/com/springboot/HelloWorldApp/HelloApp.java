e9eea36cdf99e5d4ad65fa7b0a501923ec7f256c
Tarun Dubey <tarun2019dubey@gmail.com>
2025-02-22 10:58:41 +0530

    [tarun] Add. hello app

--- src/main/java/com/springboot/HelloWorldApp/HelloApp.java
+++ src/main/java/com/springboot/HelloWorldApp/HelloApp.java
@@ -1,4 +1,4 @@
-package com.springboot.MVCcontroler;
+package com.springboot.HelloWorldApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
